b975a8368aecec26164be4c34a51ec98bca0f5a1
Sadhitra Mondal <sadhitramondal402@gmail.com>
2025-08-23 17:44:09 +0530

    from 5:48 check the project-header.tsx

--- src/modules/projects/ui/views/project-view.tsx
+++ src/modules/projects/ui/views/project-view.tsx
@@ -1,21 +1,27 @@
 "use client";
 
-import { useSuspenseQuery } from "@tanstack/react-query";
-import { useTRPC } from "@/trpc/client";
-import { json } from "stream/consumers";
+import { Suspense } from "react";
+
+import { useState } from "react";
+
+import { Fragment } from "@/generated/prisma";
 import {
     ResizableHandle,
     ResizablePanel,
     ResizablePanelGroup,
 } from "@/components/ui/resizable";
+import { ProjectHeader } from "../components/project-header";
 import { MessagesContainer } from "@/modules/projects/ui/components/messages-container";
-import { Suspense } from "react";
+
+
 
 
 interface Props {
     projectId: string;
 };
 export const ProjectView = ({ projectId }: Props) => {
+
+    const [activeFragment,setActiveFragment] = useState<Fragment | null> (null);
 
     return (
         <div className="h-screen">
@@ -24,9 +30,18 @@ export const ProjectView = ({ projectId }: Props) => {
                     defaultSize={35}
                     minSize={20}
                     className="flex flex-col min-h-0"
-                >
+                > 
+                    <Suspense fallback={<p>Loading Project...</p>}>
+                    <ProjectHeader projectId={projectId}/>
+                    </Suspense>
                     <Suspense fallback={<p>Loading messages...</p>}>
-                        <MessagesContainer projectId={projectId} />
+                        <MessagesContainer 
+
+                            projectId={projectId}
+                            activeFragment = {activeFragment}
+                            setActiveFragment = {setActiveFragment}
+                        
+                        />
                     </Suspense>
                 </ResizablePanel>
                 <ResizableHandle withHandle className="bg-gray-200" />
